822d6419d78ed704056c75876a9ff43d09ba3014
AntonioDusti1 <152974953+AntonioDusti1@users.noreply.github.com>
2023-12-12 12:35:08 +0100

    Add files via upload

---
+++ Zadatak02.cs
@@ -0,0 +1,14 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int a= 10;
+		int b= 7;
+		double c= 12.5;
+		double d = 16.7;
+		double e = (a + b + c + d) / 4;
+                Console.WriteLine("prosječna vrijednost je:"+ e ); 
+	}
+}
